18d16bf1b3e62bc6e13ac0b66e2b5b1552055882
Romel Dominguez <romeldominguez2007@gmail.com>
2026-03-12 07:36:28 -0400

    Posición de los Resultados.
    Se movió la posición de los resultados para que estuvieran en un lateral del contenedor de la calculadora, también se modificó el contenedor de resultado para que se pudiera scrollear en caso de que hubiera overflow.

--- script.js
+++ script.js
@@ -1,61 +1,69 @@
-const contenedorInput = document.querySelector('.contenedor-input');
+const contenedorResultado = document.querySelector('.contenedor-resultado');
 const botonCalcular = document.querySelector('#boton-calcular');
+const botonIgual = document.querySelector('#igual');
 
 document.addEventListener('DOMContentLoaded', function(){
     botonCalcular.addEventListener('click', function(){
-        const ecuacion = document.querySelector('#input-ec').value;
-        console.log(ecuacion);
-        const resultados = Module.calcularRaizWeb(ecuacion);
-
-        let resultadosJS = [];
-        for(let i = 0; i < resultados.size(); i++){
-            resultadosJS.push(resultados.get(i));
-        }
-
-        let resultadosJS_Fixed = [];
-        resultadosJS.forEach(resultado => {
-            resultadosJS_Fixed.push(resultado.toFixed(5));
-        });
-
-        let resultadosFinal = [];
-        let resultadoAnterior = '-';
-        resultadosJS_Fixed.forEach(_resultadoJS_Fixed => {
-            if(resultadoAnterior != _resultadoJS_Fixed){
-                resultadosFinal.push(_resultadoJS_Fixed);
-                resultadoAnterior = _resultadoJS_Fixed;
-            }
-        });
-        
-        let resultadoDisplayAnterior = document.querySelector('.resultado');
-        while(resultadoDisplayAnterior){
-            resultadoDisplayAnterior.remove();
-            resultadoDisplayAnterior = document.querySelector('.resultado');
-        }
-
-
-        if(resultadosFinal[0]  == -9999.99000){
-            const errorDisplay = document.createElement('H2');
-            errorDisplay.classList.add('error');
-            errorDisplay.classList.add('resultado');
-            errorDisplay.textContent = 'Error en la sintaxis de la ecuación.';
-            contenedorInput.appendChild(errorDisplay);
-        } else {
-            const tituloResultado = document.createElement('H2');
-            tituloResultado.classList.add('exito');
-            tituloResultado.classList.add('resultado');
-            tituloResultado.textContent = 'Raíces encontradas:';
-            contenedorInput.appendChild(tituloResultado);
-    
-            resultadosFinal.forEach(_resultadoFinal => {
-                console.log(_resultadoFinal);
-                const resultadoDisplay = document.createElement('H2');
-                resultadoDisplay.classList.add('exito');
-                resultadoDisplay.classList.add('resultado');
-                resultadoDisplay.textContent = _resultadoFinal;
-                contenedorInput.appendChild(resultadoDisplay);
-            });
-        }
-
-        resultados.delete();
-    })
+        calcularResultado();
+    });
+    botonIgual.addEventListener('click', function(){
+        calcularResultado();
+    });
 })
+
+function calcularResultado(){
+    const ecuacion = document.querySelector('#input-ec').value;
+    console.log(ecuacion);
+    const resultados = Module.calcularRaizWeb(ecuacion);
+
+    let resultadosJS = [];
+    for(let i = 0; i < resultados.size(); i++){
+        resultadosJS.push(resultados.get(i));
+    }
+
+    let resultadosJS_Fixed = [];
+    resultadosJS.forEach(resultado => {
+        resultadosJS_Fixed.push(resultado.toFixed(5));
+    });
+
+    let resultadosFinal = [];
+    let resultadoAnterior = '-';
+    resultadosJS_Fixed.forEach(_resultadoJS_Fixed => {
+        if(resultadoAnterior != _resultadoJS_Fixed){
+            resultadosFinal.push(_resultadoJS_Fixed);
+            resultadoAnterior = _resultadoJS_Fixed;
+        }
+    });
+    
+    let resultadoDisplayAnterior = document.querySelector('.resultado');
+    while(resultadoDisplayAnterior){
+        resultadoDisplayAnterior.remove();
+        resultadoDisplayAnterior = document.querySelector('.resultado');
+    }
+
+
+    if(resultadosFinal[0]  == -9999.99000){
+        const errorDisplay = document.createElement('H2');
+        errorDisplay.classList.add('error');
+        errorDisplay.classList.add('resultado');
+        errorDisplay.textContent = 'Error en la sintaxis de la ecuación.';
+        contenedorResultado.appendChild(errorDisplay);
+    } else {
+        const tituloResultado = document.createElement('H2');
+        tituloResultado.classList.add('exito');
+        tituloResultado.classList.add('resultado');
+        tituloResultado.textContent = 'Raíces encontradas:';
+        contenedorResultado.appendChild(tituloResultado);
+
+        resultadosFinal.forEach(_resultadoFinal => {
+            console.log(_resultadoFinal);
+            const resultadoDisplay = document.createElement('H2');
+            resultadoDisplay.classList.add('exito');
+            resultadoDisplay.classList.add('resultado');
+            resultadoDisplay.textContent = _resultadoFinal;
+            contenedorResultado.appendChild(resultadoDisplay);
+        });
+    }
+
+    resultados.delete();
+}
